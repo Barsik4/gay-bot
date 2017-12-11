8d77a64886777de332166f058c34565ffe07ad57
shadowolf <shadowolfbusiness@gmail.com>
2017-12-11 16:52:57 +1100

    Seperating Commands and Automatic Text Detection into seperate areas.

--- index.js
+++ index.js
@@ -5,48 +5,52 @@ var prefix = "$"
 
 client.on('message', (message) => {
 
+    // Commands
+
     if (message.content.startsWith(prefix + 'trigger')) {
-        message.reply(':warning: G A Y R R I G E D :warning:');
-    }
-
-    if (message.content == 'doom') {
-        message.reply('he gay for females :joy:');
-    }
-
-    if (message.content == 'henry') {
-        message.channel.send('hm');
-    }
-
-    if (message.content == 'jexel') {
-        message.reply('he meme :joy:');
-    }
-
-    if (message.content == 'cand') {
-            message.reply('lovely person ^o^');
-    }
-
-    if (message.content == 'vex') {
-        message.reply('who me');
-    }
-
-    if (message.content == 'ellis') {
-        message.reply('a chill guy');
+      message.reply(':warning: G A Y R R I G E D :warning:');
     }
 
     if (message.content.startsWith(prefix + 'WAKEup')) {
-        message.channel.send('WAKE UP');
+      message.channel.send('WAKE UP');
     }
 
+    // Automatic Text Detection
+
+    if (message.content == 'doom') {
+      message.reply('he gay for females :joy:');
+    }
+
+    if (message.content == 'henry') {
+      message.channel.send('hm');
+    }
+
+    if (message.content == 'jexel') {
+      message.reply('he meme :joy:');
+    }
+
+    if (message.content == 'cand') {
+      message.reply('lovely person ^o^');
+    }
+
+    if (message.content == 'vex') {
+      message.reply('who me');
+    }
+
+    if (message.content == 'ellis') {
+      message.reply('a chill guy');
+    }
+    
     if (message.content == 'loki') {
-        message.channel.send('is gay');
+      message.channel.send('is gay');
     }
 
     if (message.content == 'coco') {
-        message.channel.send('awe');
+      message.channel.send('awe');
     }
 
     if (message.content == 'eudalca') {
-        message.channel.send('wants fully automated luxury gay space communism');
+      message.channel.send('wants fully automated luxury gay space communism');
     }
 
 });
